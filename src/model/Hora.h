c3629b44d0f5159f7e76a26c09c83707f9c3c76d
José Riquelme sepúlveda <jose@MBP-de-Jose.iptv.microsoft.com>
2019-02-25 00:00:17 -0300

    add

--- src/model/Hora.h
+++ src/model/Hora.h
@@ -33,13 +33,13 @@ public:
     }
     int getCiclo(){
 
-        if(hora >= 2 && hora < 5 ) return 1;
-        if(hora >= 5 && hora < 8 ) return 2;
-        if(hora >= 8 && hora < 11 ) return 3;
-        if(hora >= 11 && hora < 14 ) return 4;
-        if(hora >= 14 && hora < 17 ) return 5;
-        if(hora >= 17 && hora < 20 ) return 6;
-        if(hora >= 20 && hora < 23 ) return 7;
-        if(hora >= 23 ) return 8;
+        if(hora >= 2 && hora < 5 ) return 0;
+        if(hora >= 5 && hora < 8 ) return 1;
+        if(hora >= 8 && hora < 11 ) return 2;
+        if(hora >= 11 && hora < 14 ) return 3;
+        if(hora >= 14 && hora < 17 ) return 4;
+        if(hora >= 17 && hora < 20 ) return 5;
+        if(hora >= 20 && hora < 23 ) return 6;
+        if(hora >= 23 ) return 7;
     }
 };
